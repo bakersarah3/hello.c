fe951708dbcc8551f1175e1e1cd5a8c48d4e0b99
bakersarah3 <54368305+bakersarah3@users.noreply.github.com>
2019-08-29 15:50:19 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+* Author: Sarah Baker
+* Date: 2019/08/27
+*
+* A simple hello world program in C
+*
+*/
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+ printf("Sarah Baker\n");
+
+ return 0;
+}
